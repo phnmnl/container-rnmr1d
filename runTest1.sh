53fb7613b4a6ed162676a73221f420149e6d2626
Kristian Peters <kristian@korseby.net>
2017-06-21 10:09:07 +0200

    updated testing script

--- runTest1.sh
+++ runTest1.sh
@@ -12,28 +12,21 @@ wget -O /data/input/MTBLS1.zip 'https://bitbucket.org/nmrprocflow/rnmr1d/raw/8ec
 # Fetch parameter files
 wget -O /data/input/NP_macro_cmd_MTBLS1.txt 'https://bitbucket.org/nmrprocflow/rnmr1d/raw/8ec5631f3ef15cd61a4906067a4de72e658787e6/examples/input/MTBLS1/NP_macro_cmd_MTBLS1.txt'
 
-# Fetch test data
-wget -O /data/expected/MTBLS1_nmrML.zip 'https://bitbucket.org/nmrprocflow/rnmr1d/raw/8ec5631f3ef15cd61a4906067a4de72e658787e6/examples/input/MTBLS1/ADG19007_nmrML.zip'
+# Fetch test files
+wget -O /data/input/test_data_matrix.txt.gz 'https://github.com/phnmnl/container-rnmr1d/raw/develop/test_data_matrix.txt.gz'
 
 # Run test
 /opt/rnmr1d/exec/Rnmr1D --zip /data/input/MTBLS1.zip \
    --proccmd /data/input/NP_macro_cmd_MTBLS1.txt \
    --outnorm PQN \
    --cpu 8 \
-   --outdir /data/output | tee /opt/output/stdout.log
+   --outdir /data/output
 
 # Compare results
-#if [[ "$(cat data_spectra.csv | head -n 1)" != "$(zcat test_data_spectra.csv.gz | head -n 1)" ]]; then
-#	echo "Test failed! Results do not match test data."
-#	exit 1
-#else
-#	echo "Test succeeded successfully."
-#fi
-
-#if [[ "$(cat data_fid.csv | head -n 1)" != "$(zcat test_data_fid.csv.gz | head -n 1)" ]]; then
-#	echo "Test failed! Results do not match test data."
-#	exit 1
-#else
-#	echo "Test succeeded successfully."
-#fi
+if [[ "$(cat /data/output/data_matrix.txt)" != "$(zcat /data/input/test_data_matrix.txt.gz)" ]]; then
+	echo "Test failed! Results do not match test data."
+	exit 1
+else
+	echo "Test succeeded successfully."
+fi
 
